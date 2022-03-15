1cf862bdeee3f9a43ed47ba80b6c0c92e37b774d
a5-stable <sh07e1916@gmail.com>
2022-03-15 23:35:26 +0900

    add comments for rack

--- application.rb
+++ application.rb
@@ -1,7 +1,9 @@
 class Application
+
+  # Rack compatible app
+  # this uses rack interface 
   def call(env)
-    [200, {}, ["Hello World: " + Time.now.to_s]]
+    # [respose_code, headers[Hash], response_body[Array]]
+    [200, {}, ["Hello World"]]
   end
 end
-
-app = Application.new
